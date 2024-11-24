4584dc64799dc2abdab541d2fb96fc9d036cc7d6
elrilacho <camilobr17@gmail.com>
2024-11-24 16:00:44 -0500

    CFA VINCULADO

--- src/config/global.js
+++ src/config/global.js
@@ -196,7 +196,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228117_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
